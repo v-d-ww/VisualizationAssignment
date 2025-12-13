2d0026d1db9da89e2d7cff4b39d14c18367bc66f
v-d-ww <2044309490@qq.com>
2025-12-13 14:30:02 +0800

    init

---
+++ README.md
@@ -0,0 +1,35 @@
+# ThreeJS 实现 3D 地图
+
+## 效果图
+
+![3dmap](/demo/3dmap.gif)
+
+## 运行
+
+```bash
+npm i && npm start
+```
+
+## 功能项
+
+- [x] 适配 resize 事件
+- [x] 地图下钻和过渡动画
+- [x] 扩散点
+- [x] 轨迹线
+- [x] 雷达
+- [x] 点光源
+- [x] Line2
+- [x] 加载 glb 动画模型
+- [x] 根据地图外接矩形动态缩放大小
+- [x] 新增 Dat GUI
+
+## 技术栈
+
+- React
+- ThreeJS
+
+## 知识点
+
+- ThreeJS 基础知识
+- 墨卡托投影转换
+- Shadar
---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
+import Map3D, { ProjectionFnParamType } from "./map3d";
+import { GeoJsonType } from "./map3d/typed";
+
+// 地图放大倍率
+const MapScale: any = {
+  province: 100,
+  city: 200,
+  district: 300,
+};
+
+function App() {
+  const [geoJson, setGeoJson] = useState<GeoJsonType>();
+  const [mapAdCode, setMapAdCode] = useState<number>(100000);
+  const [projectionFnParam, setProjectionFnParam] =
+    useState<ProjectionFnParamType>({
+      center: [104.0, 37.5],
+      scale: 40,
+    });
+
+  useEffect(() => {
+    queryMapData(mapAdCode); // 默认的中国adcode码
+  }, [mapAdCode]);
+
+  // 请求地图数据
+  const queryMapData = useCallback(async (code: number) => {
+    const response = await axios.get(
+      `https://geo.datav.aliyun.com/areas_v3/bound/${code}_full.json`
+    );
+    const { data } = response;
+    setGeoJson(data);
+  }, []);
+
+  // 双击事件
+  const dblClickFn = (customProperties: any) => {
+    setMapAdCode(customProperties.adcode);
+    setProjectionFnParam({
+      center: customProperties.centroid,
+      scale: MapScale[customProperties.level],
+    });
+  };
+
+  return (
+    <>
+      {geoJson && (
+        <Map3D
+          geoJson={geoJson}
+          dblClickFn={dblClickFn}
+          projectionFnParam={projectionFnParam}
+        />
+      )}
+    </>
+  );
+}
+
+export default App;
